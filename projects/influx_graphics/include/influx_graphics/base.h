927f9923089e1bcf2baaac610d96532123d39d8e
Arne Van Kerschaver <arnevankerschaver@gmail.com>
2024-01-11 17:06:44 +0000

    Update base.h

--- projects/influx_graphics/include/influx_graphics/base.h
+++ projects/influx_graphics/include/influx_graphics/base.h
@@ -1,10 +1,5 @@
 #pragma once
 
-#include <d3d12.h>
-#include <dxgi1_6.h>
-#include <D3Dcompiler.h>
-#include "d3dx12.h"
-
 namespace influx::graphics
 {
 	// interface base class for each object created by our graphics api
