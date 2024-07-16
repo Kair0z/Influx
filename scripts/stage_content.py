ac452516b74888995d9d8a65e372d44c2ed558e7
Arne Van Kerschaver <arnevankerschaver@gmail.com>
2024-07-16 11:38:26 +0100

    Update stage_content.py

--- scripts/stage_content.py
+++ scripts/stage_content.py
@@ -1,8 +1,14 @@
 import shutil, os
 
-print("staging content...")
 influx_root = "../"
 influx_resources = influx_root + "/resources/"
+influx_assets = influx_root + "/assets/"
 influx_staged = influx_root + "/staged/"
 influx_staged_resources = influx_staged + "/resources/"
+influx_staged_assets = influx_staged + "/assets/"
+
+print("staging resources...")
 shutil.copytree(influx_resources, influx_staged_resources, dirs_exist_ok=True)
+
+print("staging assets...")
+shutil.copytree(influx_assets, influx_staged_assets, dirs_exist_ok=True)
