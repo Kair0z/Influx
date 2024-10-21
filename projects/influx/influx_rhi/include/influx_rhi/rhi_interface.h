008d14c10e2a839fce29ae28db92c6500ef67d31
Arne Van Kerschaver <arnevankerschaver@gmail.com>
2024-10-21 18:11:07 +0100

    Update rhi_interface.h

--- projects/influx/influx_rhi/include/influx_rhi/rhi_interface.h
+++ projects/influx/influx_rhi/include/influx_rhi/rhi_interface.h
@@ -11,8 +11,10 @@ namespace influx::rhi
         using dx12_type = _dxtype;
         using vulkan_type = _vktype;
 
-        virtual ~wrapper();
-
+        void        on_switch_api(e_api_type);
+        e_api_type  get_api_type() const;
+        virtual     ~wrapper();
+        
     private:
         shared_ptr<dx12_type> m_dx12_object;
         shared_ptr<vulkan_type> m_vulkan_object;
