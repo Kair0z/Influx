8aaaa86fdd4f4dcb1cdb908afbfc1a08c94f7b99
Arne Van Kerschaver <arnevankerschaver@gmail.com>
2025-01-22 17:21:23 +0000

    Update main.cpp

--- projects/apps/rhi_tests/source/main.cpp
+++ projects/apps/rhi_tests/source/main.cpp
@@ -27,23 +27,21 @@ int main()
 	swpchain_desc.m_num_buffers = 3u;
 	graphics::swapchain* swapchain = device->create_swapchain(queue, *window, swpchain_desc);
 
-	static rendergraph::rgrendertarget_id rt_ids[3u]{};
 	static int buffer_index = 0u;
 
 	while (true)
 	{
 		rendergraph::rendergraph graph{ device };
-		graph.import_texture(RGNAME_IDX("swapchain_buffer", 0u), swapchain->get_backbuffer_resource(0u));
-		graph.import_texture(RGNAME_IDX("swapchain_buffer", 1u), swapchain->get_backbuffer_resource(1u));
-		graph.import_texture(RGNAME_IDX("swapchain_buffer", 2u), swapchain->get_backbuffer_resource(2u));
-
 		buffer_index = swapchain->acquire_backbuffer();
 
+		graph.import_texture(RGNAME("backbuffer"), swapchain->get_current_backbuffer_resource());
+
+		// clear backbuffer pass
 		graph.add_pass(
 			[](rendergraph::rgpass_builder& builder)
 			{
 				rendergraph::rgaccess access{};
-				rt_ids[buffer_index] = builder.write_rendertarget(RGNAME_IDX("swapchain_buffer", buffer_index), access);
+				builder.write_rendertarget(RGNAME("backbuffer"), access);
 			},
 			[&graph](rendergraph::rgpass_context& ctx)
 			{
@@ -51,10 +49,26 @@ int main()
 				// influx::graphics::descriptor_handle rtv_handle = ctx.get_rtv(rt_id);
 			});
 
+		// render triangle pass...
+		static rendergraph::rgrendertarget_id backbuffer_rt{};
+		graph.add_pass(
+			[](rendergraph::rgpass_builder& builder)
+			{
+				rendergraph::rgaccess access{};
+				backbuffer_rt = builder.write_rendertarget(RGNAME("backbuffer"), access);
+			},
+			[&graph](rendergraph::rgpass_context& ctx)
+			{
+
+			});
+
+
 		graph.build();
 
 		commandlist->start(device);
 		graph.execute(commandlist);
+
+		// transition backbuffer to present
 		swapchain->get_current_backbuffer_resource()->transition(commandlist, graphics::e_resource_state::present);
 		commandlist->end();
 
